b8f1408fd4d73cc5c1a2042963d1bd376cb48838
Kyle.C <k.chudobiak271@mybvc.ca>
2024-06-20 20:51:34 -0600

    Final commit for Project Milestone 1
    Added dash animation and function to player.
    Added comments to scripts for better readability.
    
    Version of Unity being used for project: 2022.3.27f1

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    [Header("Camera Settings")]
     [SerializeField]
     private float _followSpeed = 0.1f;
     [SerializeField]
     private Vector3 _offSet;
 
-
-    void FixedUpdate()
+    //Follows the player by using the players current position and follows based on a set follow speed.
+    void Update()
     {
         transform.position = Vector3.Lerp(transform.position, PlayerMovement.Instance.transform.position + _offSet, _followSpeed); //updates camera position.
     }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,25 +4,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D rb;
-
+    [Header("Movement Settings")]
     [SerializeField]
-    private float _movementSpeed;
-    
+    private float _movementSpeed = 5f;
     private float _xAxis;
-    
+
+    [Header("Ground Check Settings")]
     [SerializeField]
-    private float _jumpForce;
+    private float _jumpForce = 45f;
     [SerializeField]
     private Transform _groundCheck;
     [SerializeField]
     private LayerMask _groundLayer;
 
+    [Header("Coyote Time Settings")]
     [SerializeField]
     private float _coyoteTime;
     private float _coyoteTimeCounter;
+
+    private Rigidbody2D rb;
     public static PlayerMovement Instance;
-    Animator anim;
+    Animator animator;
+
+    [Header("Dash Settings")]
+    [SerializeField]
+    private float _dashingPower;
+    [SerializeField]
+    private float _dashingTime;
+    [SerializeField]
+    private float _dashingCoolDown;
+    private bool _canDash = true;
+    private bool _isDashing;
 
     private void Awake()
     {
@@ -39,24 +51,31 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
 
     
     void Update()
     {
+        if (_isDashing)
+        {
+            return;
+        }
         GetInput();
         MovePlayer();
         CoyoteTimer();
         Jump();
         Flip();
+        StartDash();
     }
 
+    //Gets the directional input of the player, and sets it into a variable.
     void GetInput()
     {
         _xAxis = Input.GetAxisRaw("Horizontal");
     }
 
+    //Flips the sprite of the character based on which direction the character is moving.
     void Flip()
     {
         if (_xAxis < 0)
@@ -69,17 +88,45 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    //Moves the player using the directional input variable and multiples it to the movement speed.
     private void MovePlayer()
     {
         rb.velocity= new Vector2(_movementSpeed * _xAxis, rb.velocity.y);
-        anim.SetBool("Running", rb.velocity.x != 0 && IsGrounded());
+        animator.SetBool("Running", rb.velocity.x != 0 && IsGrounded());
     }
 
+    //Executes the coroutine when the player presses the dash button and if the player can dash.
+    void StartDash()
+    {
+        if (Input.GetButtonDown("Dash") && _canDash)
+        {
+            StartCoroutine(Dash());
+        }
+    }
+
+    //Sets up the dash coroutine.
+    private IEnumerator Dash()
+    {
+        _canDash = false;
+        _isDashing = true;
+        animator.SetTrigger("Dashing");
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * _dashingPower, 0f);
+        yield return new WaitForSeconds(_dashingTime);
+        rb.gravityScale = originalGravity;
+        _isDashing = false;
+        yield return new WaitForSeconds(_dashingCoolDown);
+        _canDash = true;
+    }
+
+    //Checks if the player is on a object with a Ground layer.
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(_groundCheck.position, 0.2f, _groundLayer);
+        return Physics2D.OverlapCircle(_groundCheck.position, 0.15f, _groundLayer);
     }
 
+    //Allows the player to still jump for a small amount of time, even if the player is not Grounded.
     void CoyoteTimer()
     {
         if (IsGrounded())
@@ -92,6 +139,7 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    //Allows the player to jump when the jump key is pressed and if other conditions are met.
     void Jump()
     {
         if(Input.GetButtonUp("Jump") && rb.velocity.y > 0)
@@ -104,6 +152,6 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector3(rb.velocity.x, _jumpForce);     
         }
-        anim.SetBool("Jumping", !IsGrounded());
+        animator.SetBool("Jumping", !IsGrounded());
     }
 }
