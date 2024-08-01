99b23c460805e9d5237ea0f48d9aa3a0b3d4d0c8
Kyle.C <k.chudobiak271@mybvc.ca>
2024-08-01 14:04:27 -0600

    Final commit for final Submission
    Did some organizing with the asset sorting layers and tweaked the EndLevel game object script.
    
    Version of Unity being used for project: 2022.3.27f1

--- Assets/Scripts/EndLevelScript.cs
+++ Assets/Scripts/EndLevelScript.cs
@@ -7,33 +7,24 @@ public class EndLevelScript : MonoBehaviour
 {
 
     [SerializeField]
-    public GameObject Winner;
-    public Behaviour _disableScript;
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject _youWinText;
+    public Behaviour _disablePlayerScript;
+    [SerializeField]
+    private int _timeUntilLevelResets;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.name == "Player")
         {
-            _disableScript.enabled = false;
-            Winner.SetActive(true);
+            _disablePlayerScript.enabled = false;
+            _youWinText.SetActive(true);
             StartCoroutine (EndGame());
         }
     }
 
     private IEnumerator EndGame()
     {
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(_timeUntilLevelResets);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
